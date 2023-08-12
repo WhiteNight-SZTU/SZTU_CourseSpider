51ba032a6437eb1dc5036dbc429306f29c20fa4f
White Night <127086499+WhiteNight-SZTU@users.noreply.github.com>
2023-08-12 23:57:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,27 +29,20 @@ async function simulateLogin() {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        // 访问登录页面
         await page.goto(loginUrl);
 
-        // 输入用户名和密码
         await page.type('input[name="j_username"]', username);
         await page.type('input[name="j_password"]', password);
 
-        // 点击登录按钮
         await page.click('#loginButton');
 
-        // 等待页面加载完成
         await page.waitForNavigation();
 
-        // 打开目标网页
         await page.goto(targetUrl);
 
-        // 等待一段时间，确保页面内容已加载完毕
         await page.waitForTimeout(2000);
         console.log('已连接至教务系统，正在寻找资源...');
 
-        // 触发指定的 <li> 元素的点击事件
         await page.evaluate((selector) => {
             const li = document.querySelector(selector);
             li.click();
@@ -57,10 +50,8 @@ async function simulateLogin() {
 
         console.log('已触发响应事件，尝试保存网页资源');
 
-        // 等待一段时间，确保内容已加载完毕
         await page.waitForTimeout(2000);
 
-        // 获取链接的内容
         const content = await page.evaluate(async () => {
             const response = await fetch('https://jwxt.sztu.edu.cn/jsxsd/xskb/xskb_list.do');
             return response.text();
@@ -69,19 +60,14 @@ async function simulateLogin() {
         const timetableContent = $('#timetable').html();
         const courses = [];
 
-        // 获取所有的<td>元素
         const tdElements = $('td');
 
-        // 遍历每个<td>元素
         tdElements.each((index, tdElement) => {
-            // 获取当前<td>元素下的所有<div>元素
             const divElements = $(tdElement).find('div');
 
-            // 遍历每个<div>元素
             divElements.each((index, divElement) => {
                 const fontElements = $(divElement).find('font');
 
-                // 提取课程信息
                 const course = {
                     '课程名称': $(fontElements[0]).text(),
                     '任课教师': $(fontElements[1]).text(),
@@ -91,28 +77,23 @@ async function simulateLogin() {
                     '上课节次': extractCourseTime($(fontElements[2]).text()),
                 };
 
-                // 判断课程是否为空
                 const isEmpty = Object.values(course).every((value) => value.trim() === '');
 
-                // 如果课程不为空，将其添加到课程列表中
                 if (!isEmpty && course['上课地点'].trim() !== '' && course['上课教室'].trim() !== '') {
                     courses.push(course);
                 }
             });
         });
 
-        // 按照课程周次范围划分课程
         const courseListByWeek = groupCoursesByWeek(courses);
 
-        // 将划分后的课程列表保存为 JSON 文件
         fs.writeFileSync('timetable.json', JSON.stringify(courseListByWeek, null, 2));
         console.log('已保存筛选后的课程文件（json）');
 
-        // 将内容保存到本地文件
         fs.writeFileSync(outputFilePath, timetableContent);
         console.log('html内容已保存到文件:', outputFilePath);
 
-        // 关闭浏览器
+
         await browser.close();
         check();
 
@@ -121,19 +102,18 @@ async function simulateLogin() {
     }
 }
 
-// 提取课程周次的函数
 function extractCourseWeek(weekString) {
     const match = weekString.match(/\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*(?:\(周\)|\(双周\)|\(单周\))/);
     return match ? match[0] : '';
 }
 
-// 提取课程节次的函数
+
 function extractCourseTime(timeString) {
     const match = timeString.match(/\[(.*?)\]/);
     return match ? match[1] : '';
 }
 
-// 提取课程周次范围的函数
+
 function extractWeekRange(weekString) {
     const ranges = weekString.split(',');
     const weekRange = [];
@@ -150,7 +130,6 @@ function extractWeekRange(weekString) {
     return weekRange;
 }
 
-// 生成周次列表的函数
 function generateWeekList(weekRange) {
     const weekList = [];
 
@@ -161,7 +140,7 @@ function generateWeekList(weekRange) {
     return weekList;
 }
 
-// 根据课程周次范围划分课程
+
 function groupCoursesByWeek(courses) {
     const courseListByWeek = {};
 
@@ -190,5 +169,4 @@ function check() {
     }
 }
 
-// 开始模拟登录
 simulateLogin();
